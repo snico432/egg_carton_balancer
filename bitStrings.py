00afb1eb82f45020ab60bf194f4d3c042a080661
Sebastian Nicolas <sebnico@Sebastians-MacBook-Air-4.local>
2023-11-27 13:45:33 -0500

    renamed main.py to bitStrings.py

---
+++ bitStrings.py
@@ -0,0 +1,26 @@
+import itertools
+
+## Generates all bit strings of length n
+def genBitString(n):
+    result = ["".join(seq) for seq in itertools.product("01", repeat=n)]
+    return result
+
+## Generates all bit strings of length 1 to n
+def genAllBitStrings(n):
+    bStrings = {}
+    for i in range(1, n+1):
+        bStrings[i] = (genBitString(i))
+
+    return bStrings
+
+if __name__ == "__main__":
+    n = 0
+    while (n<=0):
+        n = int(input("Enter a positive integer: "))
+    result = genAllBitStrings(n)
+    print(result)
+    for i in range(1, n+1):
+        print(f"Length {i}: ")
+        for num in result[i]:
+            print(num)
+        print()
